c9e861e279088f082aa548d08f2fb56ae945a866
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-22 17:34:11 +0530

    operations done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -36,6 +36,40 @@
 // empty string => false
 
 // String()
-let a=true;
-let b = String(a)
-console.log(b);
+// let a=true;
+// let b = String(a)
+// console.log(b);
+
+// ***************************Operations**************************
+
+let val = 3
+let negVal = -val
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**8);
+// console.log(2/8);
+// console.log(8%3);
+
+// let str1 = "SAGAR "
+// let str2 = "Narigara"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
