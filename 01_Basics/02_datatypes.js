963858568a4b4e09a479f9685b096e45453dec45
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-22 12:29:33 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //code treated as newer version js
+
+// alert("hello")
+// We're using node.js not browser so alert wont work
+
+let name = "sagar" //string
+let age = 19 //number
+let isLoggedIn = true //boolan
+
+// DATATYPES  
+    // number
+    // bigint
+    // string => '' or ""
+    // boolean => true or false
+    // null => standalone value
+    // undefined 
+    // symbol => uniqueness
+    
+// OBJECT
+console.log(typeof "sagar");
+console.log(typeof age);
+console.log(typeof null); //Null is a object
+console.log(typeof undefined);
