949fb140abc53406a8c7851c3e02d649a80e0d31
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-22 12:07:01 +0530

    varible learn

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const id = 123
+var name = "sagar"
+let ps = "s123"
+city = "amreli"
+let a
+
+console.log(id);
+
+// cant change const varables
+
+/*
+dont use var 
+beacuse of the block scope problem
+*/
+
+console.table([id, name, ps, city, a])
