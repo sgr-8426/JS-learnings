532eb7f154f386d0d905a16e312cc90b3233b7a3
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-23 20:56:16 +0530

    objects and arrays done

--- datatypes-summery.js
+++ datatypes-summery.js
@@ -1,16 +1,28 @@
 // Primitive
 // 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
+// JS is dynamically typed
+
+// const a = Symbol("abc") 
+// const b = Symbol("abc")
+
+// console.log(a);
+// console.log(b);
+// console.log( a === b );
+
+// let c = 123423412414234124141224n //bigInt
+
+
+
+
 // Non primitive / Reference Type
 // Arrays, Objects, Functions
 
-// JS is dynamically typed
+// let Heroes = ["shaktiman", "ironman", "batman", "spiderman"] //array
 
-const a = Symbol("abc") 
-const b = Symbol("abc")
-
-console.log(a);
-console.log(b);
-console.log( a === b );
-
-let c = 123423412414234124141224n //bigInt
+// Object
+// let obj = {
+//     key: Value,
+//     name: "sagar",
+//     age: 19,
+// }
