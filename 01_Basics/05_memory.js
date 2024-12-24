9c9abbb295797c676d3eb33d4c37a03c2ccb0276
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-24 15:23:18 +0530

    Memory: heap and stack done

---
+++ 01_Basics/05_memory.js
@@ -0,0 +1,27 @@
+// Stack memory => Primitive datatypes
+// Heap memory => Non-Primitive datatypes
+
+// Stack
+let myName = "sagar"
+let myName2 = myName
+
+myName2 = "bravo"
+
+console.log(myName);
+console.log(myName2);
+
+
+
+
+// Heap
+let user = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let user2=user
+
+user2.email = "sagar@gmail.com"
+
+console.log(user.email);
+console.log(user2.email);
