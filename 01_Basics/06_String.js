65190b15abc52e51aba5818c4978acbcf4ddfa59
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-25 23:09:16 +0530

    half of string done

---
+++ 01_Basics/06_String.js
@@ -0,0 +1,16 @@
+const name = "sagar"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my erepo count is ${repoCount}`);
+
+const gameName = new String('Sagar N')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
