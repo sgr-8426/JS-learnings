12eaab4e8f4d4958c13d52eba75ed585e2111aa3
sgr-8426 <sagarnarigara69@gmail.com>
2024-12-31 17:40:12 +0530

    numbers done

---
+++ 01_Basics/07_numbers.js
@@ -0,0 +1,20 @@
+const number = 42;
+// console.log(number);
+
+const balance = new Number(2242.89840582305);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+// console.log(balance.toPrecision(6));
+// console.log(balance.toPrecision(3));
+
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+
+const hundreds = 100000000000;
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString(`en-IN`));
+
+// go to console any check Number.MIN_VALUE and Number.MAX_VALUE and Number.someMethod()
