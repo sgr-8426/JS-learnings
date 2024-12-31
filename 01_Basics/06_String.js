8828dd0e4f666ec121d70b605432d32eb266f268
sgr-8426 <sagarnarigara69@gmail.com>
2024-12-31 15:52:13 +0530

    15:02-11

--- 01_Basics/06_String.js
+++ 01_Basics/06_String.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 // console.log(`Hello my name is ${name} and my erepo count is ${repoCount}`);
 
-const gameName = new String('Sagar N')
+const gameName = new String('Sagar-N')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,4 +13,16 @@ const gameName = new String('Sagar N')
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(3));
-console.log(gameName.indexOf('a'));
+// console.log(gameName.indexOf('a'));
+
+// cant parse negative value to substring method
+// does not include the last index
+// if neagtive value given it will be considered as 0
+// console.log(gameName.substring(0, 4));
+
+// can parse negative value to slice method and result starts from the end
+// first in positive and second is negative, negative starts from -1 means last element
+// does not include the last index
+// if first parsing is negative it will be considered as 0
+console.log(gameName.slice(5, -6));
+
