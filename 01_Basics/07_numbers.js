03f03bb745cb0c65bfc0b77657f31199cd8f9424
sgr-8426 <sagarnarigara69@gmail.com>
2025-01-19 14:27:42 +0530

    numbers

--- 01_Basics/07_numbers.js
+++ 01_Basics/07_numbers.js
@@ -14,7 +14,27 @@ const balance = new Number(2242.89840582305);
 // console.log(balance.toString().length);
 
 const hundreds = 100000000000;
-console.log(hundreds.toLocaleString());
-console.log(hundreds.toLocaleString(`en-IN`));
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString(`en-IN`));
 
 // go to console any check Number.MIN_VALUE and Number.MAX_VALUE and Number.someMethod()
+
+
+// +++++++++++++++++++++++++++++++++++++++++Math
+
+// console.log(Math);
+// go to console and check Math object
+
+// make number positive
+// console.log(Math.abs(-42));
+
+// console.log(Math.round(4.1));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+
+// console.log(Math.min(1, 2, 3, 4, 5));
+// console.log(Math.max(1, 2, 3, 4, 5));
+
+// value between 0 and 1
+// console.log(Math.random());
+
