ffd19c05447766b27befed4ffa4e9a4eb6ef2741
sgr-8426 <sagarnarigara69@gmail.com>
2025-01-28 16:00:37 +0530

    some things remains in array but for now its done

--- 10_arrays.js
+++ 10_arrays.js
@@ -47,7 +47,42 @@ const myArray3 = myArray.join();
 
 
 // splice
-console.log("before using splice original array ", myArray);
+// console.log("before using splice original array ", myArray);
 const myArray5 = myArray.splice(1, 3);
-console.log("splice ", myArray5);
-console.log("after using splice original array ", myArray);
+// console.log("splice ", myArray5);
+// console.log("after using splice original array ", myArray);
+
+
+// ---------------------------------------------------------------------------------
+
+const marvel_heroes = ["spiderman", "ironman", "thor", "hulk"];
+const dc_heroes = ["batman", "superman", "flash"]
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[4][1]);
+
+// const all_heroes = marvel_heroes.concat(dc_heroes);
+// console.log(all_heroes);
+// or
+const all_new_heroes = [...marvel_heroes, ...dc_heroes];
+// console.log(all_new_heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6],7,[6,7,[4,5]]];
+const flat_array = another_array.flat(Infinity); //give depth as parameter
+// console.log(flat_array);
+
+// console.log(Array.isArray("sagar"));
+// console.log(Array.from("sagar"));
+
+// interesting
+// console.log(Array.from({
+//     name: "sagar",
+//     age: 19,
+// }));
+
+let score1=100
+let score2=200
+let score3=300
+// console.log(Array.of(score1,score2,score3));
+
